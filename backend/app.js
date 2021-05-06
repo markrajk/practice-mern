9e476f09ac13023c0604d3a355ff6a67d0a82ee2
markrajk <marko88rajkovic@gmail.com>
2021-05-06 09:21:20 +0200

    app.js update 01

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,6 @@ if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'))
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello from server')
-})
-
 // Middleware to log cookies and time of req
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
@@ -29,6 +25,20 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/v1/users/', userRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`))
